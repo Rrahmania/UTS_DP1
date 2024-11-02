ad65cfe39cd9dc87408ef10b8d24fa4866f030e2
Rrahmania <rahmaniautami34@gmail.com>
2024-11-02 20:03:30 +0700

    Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,26 @@
-//ketik kode jawaban noo 3 kamu disini
+program bintang;
+uses crt;
+var
+    a,b,c:integer;
+begin
+    clrscr;
+    //untuk memasukan angka yang diminta oleh user
+    write('masukan angka : ');
+    readln(a);
+    //untuk pengulangan nya
+    for b := 1 to a do
+    begin
+    for c := 1 to a do
+        if (c = b) or (c = (a-b+1)) then
+        //untuk pengulangan menulis bintang
+        begin
+            write('*');
+        end
+        //untuk pengulangan untuk menulis spasi
+        else
+        begin
+            write(' ');
+        end;
+        writeln;
+        end;
+end.
